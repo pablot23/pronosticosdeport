a6454812297028b2735992363f145a98d1f8695f
WillyEsco <90744366+WillyEsco@users.noreply.github.com>
2023-03-19 10:36:04 -0300

    leer resultados

--- src/App/App.java
+++ src/App/App.java
@@ -9,20 +9,29 @@ import java.util.List;
 public class App {
 
     // public  List<Partido> partidosList = new ArrayList<Partido>();
-   // public  List<Pronostico> pronosticosList = new ArrayList<Pronostico>();
+    // public  List<Pronostico> pronosticosList = new ArrayList<Pronostico>();
 
     public static  void main(String[] args) throws Exception {
 
         // Leo los resultados y obtengo un array de los pronosticos
         List<Partido> partidosList = leerResultados();
-        List<Pronostico> pronosticosList = leerPronosticos(partidosList);
-        
-        // Calculo los puntos
-        int puntos= 0;
-        for (Pronostico p : pronosticosList ) {
-             puntos += p.getPuntos();
+        for (Partido p : partidosList ) {
+            System.out.println(p.toString());
         }
-        System.out.println("Puntos obtenidos: " + puntos);        
+        // List<Pronostico> pronosticosList = leerPronosticos(partidosList);
+
+        // // Calculo los puntos
+        // int puntos= 0;
+        // if (pronosticosList != null) {
+        //     for (Pronostico p : pronosticosList ) {
+        //         puntos += p.getPuntos();
+        //     }
+        //     System.out.println("Puntos obtenidos: " + puntos);  
+        // }else{
+        //     System.out.println("No se pudo leer el archivo de pronosticos");
+        // }
+        
+       
     }
 
     static List<Partido> leerResultados() {
@@ -31,32 +40,59 @@ public class App {
         int nroDeMatch = 0; // inicializo en 0, por def el archivo viene en orden
        // Ronda ronda = new Ronda(nroDeRonda, null);
         Scanner scResu = null;
-   
-        File resultados = new File("C:\\Users\\Willy\\Desktop\\TrabajoPrácticoIntegrador\\PronosticosDeportivos\\PronDep\\resultados.csv");
+        String[] datos = null;
+        File resultados = new File("C:\\Users\\Willy\\Desktop\\VSCProjects\\PronosticosDeportivos\\src\\App\\resultados.csv");
+        
         // leer archivo resultados.cvs y lo guardo en una coleccion de objetos Partido
         List<Partido> partidosList = new ArrayList<Partido>();
         try {
             scResu = new Scanner(resultados);
+            scResu.nextLine(); // salteo encabezados
+            System.out.println();
+              //  Ejemplo: 1;Argentina;Seleccionado;1;2;2;Arabia Saudita;Seleccionado
+                int id_equip1 = 0;
+                String equipo1 = "";
+                String descrip1 = "";
+                int goles1 = 0;
+                int goles2 = 0;
+                int id_equip2 = 0;
+                String equipo2 = "";
+                String descrip2 = "";
+                //preparo 2 variables auxiliares para crear los objetos de los equipos
+                Equipo equipo1Aux;
+                Equipo equipo2Aux;
+
+           
             while (scResu.hasNextLine()) {
                 nroDeMatch++;        
                 // Objetivo1, crear los objetos de la info que se encuentran en el archivo resultados.csv
-                String[] datos = scResu.nextLine().split(",");
+                datos = scResu.nextLine().split(";");
                 // leo datos
 
-                int    goles1  = Integer.parseInt(datos[1]);
-                int    goles2  = Integer.parseInt(datos[3]);
+                id_equip1 = Integer.parseInt(datos[0]);
+                equipo1 = datos[1];
+                descrip1 = datos[2];
+                goles1 = Integer.parseInt(datos[3]);
+                goles2 = Integer.parseInt(datos[4]);
+                id_equip2 = Integer.parseInt(datos[5]);
+                equipo2 = datos[6];
+                descrip2 = datos[7];
       
                 // agrego el partido al array de partidos
-            
-                partidosList.add(new Partido(new Equipo(datos[0], ""), new Equipo(datos[2], ""), goles1, goles2, nroDeRonda, nroDeMatch));
+                equipo1Aux = new Equipo(id_equip1,equipo1,descrip1);
+                equipo2Aux = new Equipo(id_equip2,equipo2,descrip2);
+                partidosList.add(new Partido( equipo1Aux, equipo2Aux, goles1, goles2, nroDeRonda, nroDeMatch));
             }
-            scResu.close();         
+                scResu.close();         
             return partidosList;
-        } catch (FileNotFoundException e) {
+        } 
+          catch (FileNotFoundException e) {
             e.printStackTrace();
             return null;
         } 
-    } 
+    }
+
+
 
     static List<Pronostico> leerPronosticos(List<Partido> partidosList) {
 
@@ -65,8 +101,8 @@ public class App {
    //   Ronda ronda = new Ronda(nroDeRonda, null);
       ResultadoEnum resultado = null;
 
-      Equipo equipoUno = new Equipo(null, null);
-      Equipo equipoDos = new Equipo(null, null);
+      Equipo equipoUno;
+      Equipo equipoDos;
       int indice;
       int nroDeMatch = 0;
       // inicializo variables
@@ -74,7 +110,7 @@ public class App {
 
 
       Pronostico pronostico = new Pronostico(null, null, null, nroDeRonda, 0);
-      File pronosticos = new File("C:\\Users\\Willy\\Desktop\\TrabajoPrácticoIntegrador\\PronosticosDeportivos\\PronDep\\pronosticos.csv");
+      File pronosticos = new File("C:\\Users\\Willy\\Desktop\\VSCProjects\\PronosticosDeportivos\\src\\App\\pronostico.csv");
       Scanner scPron = null;        
       List<Pronostico> pronosticosList = new ArrayList<Pronostico>();
       try {
--- src/App/Equipo.java
+++ src/App/Equipo.java
@@ -1,17 +1,26 @@
 package App;
 public class Equipo {
+    private int id;
     private String nombre;
     private String descripcion;
 
-    public Equipo(String nombre, String descripcion) {
+    public Equipo( int id, String nombre, String descripcion) {
+        this.id = id;
         this.nombre = nombre;
         this.descripcion = descripcion;
+
+    }
+    public int getId() {
+        return this.id;
     }
     public String getNombre() {
         return this.nombre;
     }
     public String getDescripcion() {
         return this.descripcion;
+    }
+    public void setId(int id) {
+        this.id = id;
     }
     public void setNombre(String nombre) {
         this.nombre = nombre;
